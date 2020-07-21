18feb48ed7f4d8b0033a171e1f66b5c90609a823
Артем <artem_lipinskiy@mail.ru>
2020-07-21 17:21:29 +0300

    Добавление логирования в DiagnosticService

--- DigitalHealth.Services/DiagnosticService.cs
+++ DigitalHealth.Services/DiagnosticService.cs
@@ -14,65 +14,89 @@ namespace DigitalHealth.Web.Services
 
     public class DiagnosticService : IDiagnosticService
     {
+        private readonly ILogger _logger;
+
+        public DiagnosticService(ILogger logger)
+        {
+            _logger = logger;
+        }
         public async Task<List<DiagnosticResultDto>> GetResult(List<Guid> SymptomIds)
         {
-            using (DHContext db = new DHContext())
+            try
             {
-                var AllDisease = await db.Diseases.Include(d => d.ICD).Include(d => d.Symptoms).ToListAsync();
-                List<DiagnosticResultDto> result = new List<DiagnosticResultDto>();
-                foreach (var disease in AllDisease)
+                using (DHContext db = new DHContext())
                 {
-                    DiagnosticResultDto currnetresult = null;
-                    foreach (var diseaseSymptom in disease.Symptoms)
+                    var AllDisease = await db.Diseases.Include(d => d.ICD).Include(d => d.Symptoms).ToListAsync();
+                    List<DiagnosticResultDto> result = new List<DiagnosticResultDto>();
+                    foreach (var disease in AllDisease)
                     {
-                        if (SymptomIds.Contains(diseaseSymptom.Id))
+                        DiagnosticResultDto currnetresult = null;
+                        foreach (var diseaseSymptom in disease.Symptoms)
                         {
-                            if (currnetresult == null)
+                            if (SymptomIds.Contains(diseaseSymptom.Id))
                             {
-                                currnetresult = new DiagnosticResultDto();
-                                currnetresult.NumberOfCoincidences = 1;
-                                var diseaseresult = new DiseaseDto
+                                if (currnetresult == null)
                                 {
-                                    Id = disease.Id,
-                                    Description = disease.Description,
-                                    ICDID = disease.ICDID,
-                                    ICDName = disease.ICD.Name,
-                                    Name = disease.Name,
-                                    SymptomIds = disease.Symptoms.Select(s => s.Id).ToList(),
-                                    SymptomNames = disease.Symptoms.Select(s => s.Name).ToList()
-                                };
-                                currnetresult.Disease = diseaseresult;
-                            }
-                            else
-                            {
-                                currnetresult.NumberOfCoincidences++;
+                                    currnetresult = new DiagnosticResultDto();
+                                    currnetresult.NumberOfCoincidences = 1;
+                                    var diseaseresult = new DiseaseDto
+                                    {
+                                        Id = disease.Id,
+                                        Description = disease.Description,
+                                        ICDID = disease.ICDID,
+                                        ICDName = disease.ICD.Name,
+                                        Name = disease.Name,
+                                        SymptomIds = disease.Symptoms.Select(s => s.Id).ToList(),
+                                        SymptomNames = disease.Symptoms.Select(s => s.Name).ToList()
+                                    };
+                                    currnetresult.Disease = diseaseresult;
+                                }
+                                else
+                                {
+                                    currnetresult.NumberOfCoincidences++;
+                                }
                             }
                         }
-                    }
 
-                    if (currnetresult != null)
-                    {
-                        result.Add(currnetresult);
+                        if (currnetresult != null)
+                        {
+                            result.Add(currnetresult);
+                        }
                     }
+                    return result.OrderByDescending(r => r.NumberOfCoincidences).ToList();
                 }
-                return result.OrderByDescending(r => r.NumberOfCoincidences).ToList();
             }
+            catch (Exception exc)
+            {
+                _logger.Error($"Failed get result for diagnostic : {exc}");
+                throw;
+            }
+           
         }
 
         public async Task<List<MethodOfTreatmentDto>> FindMethods(Guid DiseaseId)
         {
-            using (DHContext db = new DHContext())
+            try
             {
-                return await db.MethodOfTreatments.Include(m=>m.Disease).Where(m => m.DiseaseId == DiseaseId).Select(m=> new MethodOfTreatmentDto
+                using (DHContext db = new DHContext())
                 {
-                    Id = m.Id,
-                    Description = m.Description,
-                    DiseaseId = m.DiseaseId,
-                    DiseaseName = m.Disease.Name,
-                    Source = m.Source,
-                    Title = m.Title
-                }).ToListAsync();
+                    return await db.MethodOfTreatments.Include(m => m.Disease).Where(m => m.DiseaseId == DiseaseId).Select(m => new MethodOfTreatmentDto
+                    {
+                        Id = m.Id,
+                        Description = m.Description,
+                        DiseaseId = m.DiseaseId,
+                        DiseaseName = m.Disease.Name,
+                        Source = m.Source,
+                        Title = m.Title
+                    }).ToListAsync();
+                }
             }
+            catch (Exception exc)
+            {
+                _logger.Error($"Failed find methods for disease {DiseaseId} :{exc}");
+                throw;
+            }
+           
         }
     }
 }
